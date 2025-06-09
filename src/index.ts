7a7600712ef71402b7f854cbe2709a527c66c714
Dima Tretyak <itretyak@ya.ru>
2025-06-09 16:13:14 +0300

    fix: change variable declarations to const and update text color in presentation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import pptxgen from "pptxgenjs";
 
 // 1. Create a Presentation
-let pres = new pptxgen();
+const pres = new pptxgen();
 
 // 2. Add a Slide to the presentation
-let slide = pres.addSlide();
+const slide = pres.addSlide();
 
 // 3. Add 1+ objects (Tables, Shapes, etc.) to the Slide
 slide.addText("Hello World from PptxGenJS!", {
   x: 1,
   y: 1,
-  color: "363636",
+  color: "FF0000",
 });
 
 // 4. Save the Presentation
