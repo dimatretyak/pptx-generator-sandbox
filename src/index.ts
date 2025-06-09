3ae5ea4cde291ab0fb9b7d7c78d304abd7881312
Dima Tretyak <itretyak@ya.ru>
2025-06-09 17:29:22 +0300

    feat: add table and simple table slides to PresentationBuilder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import pptxgen from "pptxgenjs";
 import path from "node:path";
 
+// 16:9 aspect ratio
+const LAYOUT_NAME = "APP";
+const SLIDE_WIDTH = 10;
+const SLIDE_HEIGHT = 5.625;
+
 class PresentationBuilder {
   private slideGenerators: Array<
     (slide: pptxgen.Slide, presentation: pptxgen) => void
@@ -50,9 +55,191 @@ class PresentationBuilder {
     return this;
   }
 
+  addSimpleTableSlide() {
+    const MARGIN = 1;
+
+    this.slideGenerators.push((slide) => {
+      slide.addTable(
+        [
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+          [
+            {
+              text: "111",
+            },
+            {
+              text: "222",
+            },
+          ],
+        ],
+        {
+          autoPage: true,
+          x: MARGIN,
+          y: MARGIN,
+          margin: 0.1,
+          w: SLIDE_WIDTH - MARGIN * 2,
+          h: SLIDE_HEIGHT - MARGIN * 2,
+          border: {
+            color: "9F9F9F",
+          },
+        }
+      );
+    });
+
+    return this;
+  }
+
+  addTableSlide() {
+    this.slideGenerators.push((slide) => {
+      const rows: pptxgen.TableRow[] = [
+        [
+          {
+            text: "Top Lft",
+            options: { valign: "top", align: "left", fontFace: "Arial" },
+          },
+          {
+            text: "Top Ctr",
+            options: { valign: "top", align: "center", fontFace: "Courier" },
+          },
+          {
+            text: "Top Rgt",
+            options: { valign: "top", align: "right", fontFace: "Verdana" },
+          },
+        ],
+        [
+          { text: "Mdl Lft", options: { valign: "middle", align: "left" } },
+          { text: "Mdl Ctr", options: { valign: "middle", align: "center" } },
+          { text: "Mdl Rgt", options: { valign: "middle", align: "right" } },
+        ],
+        [
+          { text: "Btm Lft", options: { valign: "bottom", align: "left" } },
+          { text: "Btm Ctr", options: { valign: "bottom", align: "center" } },
+          { text: "Btm Rgt", options: { valign: "bottom", align: "right" } },
+        ],
+      ];
+
+      slide.addTable(rows, {
+        x: 0.5,
+        y: 1.1,
+        rowH: 0.75,
+        fill: {
+          color: "F7F7F7",
+        },
+        fontSize: 14,
+        color: "363636",
+        border: {
+          color: "BBCCDD",
+        },
+      });
+    });
+
+    return this;
+  }
+
   buildAndSave(fileName: string) {
     const presentation = new pptxgen();
-    presentation.layout = "LAYOUT_16x9";
+
+    presentation.defineLayout({
+      name: LAYOUT_NAME,
+      width: SLIDE_WIDTH,
+      height: SLIDE_HEIGHT,
+    });
+
+    presentation.layout = LAYOUT_NAME;
 
     for (const generateSlide of this.slideGenerators) {
       const slide = presentation.addSlide();
@@ -66,6 +253,8 @@ class PresentationBuilder {
 const builder = new PresentationBuilder()
   .addWelcomeSlide()
   .addMediaSlide()
-  .addShapeSlide();
+  .addShapeSlide()
+  .addSimpleTableSlide()
+  .addTableSlide();
 
 builder.buildAndSave("output/demo.pptx");
