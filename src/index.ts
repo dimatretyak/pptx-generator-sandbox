97cd0da5c8484ce135c5aa01a8e49d5fb03fa519
Dima Tretyak <itretyak@ya.ru>
2025-06-09 16:10:24 +0300

    feat: add pptxgenjs dependency and implement basic presentation creation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+import pptxgen from "pptxgenjs";
+
+// 1. Create a Presentation
+let pres = new pptxgen();
+
+// 2. Add a Slide to the presentation
+let slide = pres.addSlide();
+
+// 3. Add 1+ objects (Tables, Shapes, etc.) to the Slide
+slide.addText("Hello World from PptxGenJS!", {
+  x: 1,
+  y: 1,
+  color: "363636",
+});
+
+// 4. Save the Presentation
+pres.writeFile({
+  fileName: "assets/Hello-World.pptx",
+});
