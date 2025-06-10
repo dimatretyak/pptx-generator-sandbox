4b5a16f3525c0f0cc77b76f1cde67da8901a055f
Dima Tretyak <itretyak@ya.ru>
2025-06-10 16:23:36 +0300

    feat: add html files

--- src/index.ts
+++ src/index.ts
@@ -395,21 +395,92 @@ class PresentationBuilder {
   }
 }
 
-const builder = new PresentationBuilder().addCardsSlide([
-  [
-    { title: "Impressions", value: "177M" },
-    { title: "Clicks", value: "269K" },
-    { title: "CTR(%)", value: "0.15%" },
-  ],
-  [
-    { title: "111", value: "269K" },
-    { title: "222", value: "0.15%" },
-  ],
-  [
-    { title: "333", value: "269K" },
-    { title: "444", value: "0.15%" },
-    { title: "555", value: "0.15%" },
-  ],
-]);
-
+const builder = new PresentationBuilder()
+  .addCardsSlide([
+    [
+      { title: "Impressions", value: "177M" },
+      { title: "Clicks", value: "269K" },
+      { title: "CTR(%)", value: "0.15%" },
+    ],
+    [
+      { title: "Impressions", value: "33.3M" },
+      { title: "Site Conversions", value: "2.7K" },
+    ],
+    [
+      { title: "Foot Traffic Visits", value: "4.4K" },
+      { title: "Video Start(s)", value: "2.6M" },
+      { title: "Video Complete(s)", value: "1.3M" },
+    ],
+  ])
+  .addCardsSlide([
+    [
+      { title: "Impressions", value: "177M" },
+      { title: "Clicks", value: "269K" },
+      { title: "CTR(%)", value: "0.15%" },
+    ],
+    [
+      { title: "VCR(%)", value: "50.41%" },
+      { title: "Impressions", value: "33.3M" },
+      { title: "Site Conversions", value: "2.7K" },
+    ],
+    [
+      { title: "Foot Traffic Visits", value: "4.4K" },
+      { title: "Video Start(s)", value: "2.6M" },
+      { title: "Video Complete(s)", value: "1.3M" },
+    ],
+  ])
+  .addCardsSlide([
+    [
+      { title: "Impressions", value: "177M" },
+      { title: "Clicks", value: "269K" },
+      { title: "CTR(%)", value: "0.15%" },
+      { title: "VCR(%)", value: "50.41%" },
+    ],
+    [
+      { title: "Impressions", value: "33.3M" },
+      { title: "Site Conversions", value: "2.7K" },
+      { title: "Foot Traffic Visits", value: "4.4K" },
+      { title: "Video Start(s)", value: "2.6M" },
+    ],
+  ])
+  .addCardsSlide([
+    [
+      { title: "Impressions", value: "177M" },
+      { title: "Clicks", value: "269K" },
+      { title: "CTR(%)", value: "0.15%" },
+    ],
+    [
+      { title: "Impressions", value: "33.3M" },
+      { title: "Total Calls", value: "17.7K" },
+      { title: "Site Conversions", value: "2.7K" },
+    ],
+    [
+      { title: "Foot Traffic Visits", value: "4.4K" },
+      { title: "Video Start(s)", value: "2.6M" },
+      { title: "Video Complete(s)", value: "1.3M" },
+    ],
+    [
+      { title: "First Calls", value: "12.2K" },
+      { title: "Answered Calls", value: "15.7K" },
+      { title: "Answered (%)", value: "88.70%" },
+    ],
+  ])
+  .addCardsSlide([
+    [{ title: "Impressions", value: "177M" }],
+    [
+      { title: "Impressions", value: "33.3M" },
+      { title: "Total Calls", value: "17.7K" },
+      { title: "Site Conversions", value: "2.7K" },
+    ],
+    [
+      { title: "Foot Traffic Visits", value: "4.4K" },
+      { title: "Video Start(s)", value: "2.6M" },
+      { title: "Video Complete(s)", value: "1.3M" },
+    ],
+    [
+      { title: "First Calls", value: "12.2K" },
+      { title: "Answered Calls", value: "15.7K" },
+      { title: "Answered (%)", value: "88.70%" },
+    ],
+  ]);
 builder.buildAndSave("output/demo.pptx");
