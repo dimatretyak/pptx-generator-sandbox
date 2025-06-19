05de7e768bf1ac61ada7fe46dab0db91640630fb
Dima Tretyak <itretyak@ya.ru>
2025-06-19 17:48:48 +0300

    feat: ability to pass prev value for boxes

--- src/classes/PowerPointBoxes.ts
+++ src/classes/PowerPointBoxes.ts
@@ -7,10 +7,13 @@ import {
 } from "../types/powerpoint.types";
 import { formatValue } from "../utils/formatters";
 import { PowerPointLayout } from "./PowerPointLayout";
+import { isNumber } from "../utils/common";
 
 export type PowerPointBoxEntity = {
   title: string;
   value: PowerPointValue;
+  prevValue?: PowerPointValue;
+  changePercentage?: number;
   format?: PowerPointValueFormatter;
 };
 
@@ -49,38 +52,58 @@ export class PowerPointBoxes {
           },
         });
 
-        slide.addText(
-          [
-            {
-              text: col.title,
-              options: {
-                fontSize: 14,
-                breakLine: true,
-              },
-            },
-            {
-              text: formatValue(col.value, col.format),
-              options: {
-                fontSize: 24,
-                bold: true,
-              },
-            },
-          ],
+        const texts: pptxgen.TextProps[] = [
           {
-            shape: "roundRect",
-            x: info.x,
-            y: info.y,
-            w: info.width,
-            h: info.height,
-            align: "center",
-            fontSize: 14,
-            rectRadius: this.config.roundess,
-            line: {
-              color: this.config.border.color,
-              size: this.config.border.size,
+            text: col.title,
+            options: {
+              fontSize: 14,
+              breakLine: true,
             },
-          }
-        );
+          },
+          {
+            text: formatValue(col.value, col.format),
+            options: {
+              fontSize: 24,
+              bold: true,
+              breakLine: true,
+            },
+          },
+        ];
+
+        if (isNumber(col.changePercentage)) {
+          texts.push({
+            text: formatValue(col.changePercentage, col.format),
+            options: {
+              fontSize: 16,
+              breakLine: true,
+            },
+          });
+        }
+
+        if (isNumber(col.prevValue)) {
+          texts.push({
+            text: `vs ${formatValue(col.prevValue, col.format)} prev.`,
+            options: {
+              fontSize: 14,
+              color: "9e9e9e",
+            },
+          });
+        }
+
+        slide.addText(texts, {
+          shape: "roundRect",
+          x: info.x,
+          y: info.y,
+          w: info.width,
+          h: info.height,
+          align: "center",
+          fontSize: 14,
+          rectRadius: this.config.roundess,
+          line: {
+            color: this.config.border.color,
+            size: this.config.border.size,
+          },
+        });
       });
     });
   }
