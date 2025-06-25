b7f67902720c29adf4f9dab416f1035592992f9c
Dima Tretyak <itretyak@ya.ru>
2025-06-25 14:05:07 +0300

    chore: master slide styling

--- src/classes/PowerPointBuilder.ts
+++ src/classes/PowerPointBuilder.ts
@@ -18,6 +18,8 @@ import {
   PowerPointPieChart,
   PowerPointPieChartPayload,
 } from "./PowerPointPieChart";
+import path from "node:path";
+import { config } from "../config";
 
 // 16:9 aspect ratio
 const LAYOUT_NAME = "APP";
@@ -253,6 +255,14 @@ class PowerPointBuilder {
 
   addMasterSlide(title: string) {
     this.slideGenerators.push((slide) => {
+      slide.addImage({
+        w: this.config.slide.width,
+        h: this.config.slide.height,
+        x: 0,
+        y: 0,
+        path: path.join(config.path.images, "master-slide-bg.svg"),
+      });
+
       slide.addText(title, {
         x: 0,
         y: 0,
@@ -260,10 +270,9 @@ class PowerPointBuilder {
         h: this.config.slide.height,
         align: "center",
         rectRadius: this.config.roundess,
-        line: {
-          color: this.config.border.color,
-          size: this.config.border.size,
-        },
+        fontSize: 30,
+        bold: true,
+        color: "ffffff",
       });
     });
   }
